f5b084cc187bb7606484e31e4cc15c2bed3a14c7
Kyle Salaysay <badkittybogs@gmail.com>
2023-12-03 23:20:11 +0800

    redefined list image

--- src/components/reusables/profileCard.tsx
+++ src/components/reusables/profileCard.tsx
@@ -18,7 +18,7 @@ const ProfileCard:React.FC<ProfileCardProps> = ({ profile }) => {
 
   return (
       <div key={profile.id} className="max-w-sm h-full rounded-lg overflow-hidden shadow-lg border-1 border-blue-950">
-        <Image src={profile.profilePic} alt={profile.name} width={300} height={500}/>
+        <Image src={profile.profilePic} alt={profile.name} width={300} height={300}/>
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">{profile.name}</div>
           <p className="text-gray-700 text-base">{profile.address}</p>
